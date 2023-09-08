3f94514cf7345b8e3e1fdaa5bbbeddf96109f9e7
Benoit GRASSET <b.grasset@cliqdigital.com>
2023-09-08 14:05:39 +0200

    fix: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
 
+import { useState } from "react";
 import "./App.css";
 import Draggable from "./Draggable";
 import Droppable from "./Droppable";
 
-function App() {
+const App = () => {
+  const [isDropped, setIsDropped] = useState(false);
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  };
+
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
   return (
-    <>
-      <DndContext>
-        <Draggable />
-        <Droppable />
-      </DndContext>
-    </>
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+    </DndContext>
   );
-}
+};
 
 export default App;
